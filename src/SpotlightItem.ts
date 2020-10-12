df0b7c78b6cfd20729732773d408f9c923a78d0e
Helixar <contact@cloudnode.fr>
2020-10-12 08:47:12 +0200

    build(npm): First commit

---
+++ src/SpotlightItem.ts
@@ -0,0 +1,59 @@
+export default class SpotlightItem
+{
+    public element!: HTMLLIElement | null
+    private title: string
+    private href: string
+
+    constructor(title: string, href: string)
+    {
+        const li: HTMLLIElement = document.createElement('li')
+        const a: HTMLAnchorElement | HTMLLinkElement = document.createElement('a')
+        a.setAttribute('href', href)
+        a.innerText = title
+        li.appendChild(a)
+        li.setAttribute('hidden', 'hidden')
+        this.element = li
+        this.title = title
+        this.href = href
+        this.hide()
+    }
+
+    match(regexp: RegExp): boolean
+    {
+        const matches: RegExpMatchArray | null = this.title.match(regexp)
+        if (matches === null) {
+            this.hide()
+            return false
+        }
+        const link = this.element?.firstElementChild as HTMLLinkElement
+        link.innerHTML = matches.reduce((acc, match, index) => {
+            if (index === 0) {
+                return acc
+            }
+            console.log(this.element?.parentElement)
+            return acc + (index % 2 === 0 ? `<mark>${match}</mark>` : match)
+        }, '')
+        this.element?.removeAttribute('hidden')
+        return true
+    }
+
+    hide(): void
+    {
+        this.element?.setAttribute('hidden', 'hidden')
+    }
+
+    select()
+    {
+        this.element?.classList.add('bg-black', 'bg-opacity-25')
+    }
+
+    unselect()
+    {
+        this.element?.classList.remove('bg-black', 'bg-opacity-25')
+    }
+
+    follow()
+    {
+        window.location.href = this.href
+    }
+}
